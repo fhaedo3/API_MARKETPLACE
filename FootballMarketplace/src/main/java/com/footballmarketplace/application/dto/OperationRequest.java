0ba29bc99be304f0a6902226551eb915e0ccb46f
Santino Javier Bertoia <131382765+SantinoBertoia@users.noreply.github.com>
2025-04-08 20:45:52 -0300

    Borro archivos viejos para subir los nuevos

--- FootballMarketplace/src/main/java/com/footballmarketplace/application/dto/OperationRequest.java
+++
@@ -1,8 +0,0 @@
-package com.footballmarketplace.application.dto;
-
-import lombok.Data;
-
-@Data
-public class OperationRequest {
-    private String description; 
-}
--- FootballMarketplace/src/main/java/com/footballmarketplace/domain/enums/Role.java
+++
@@ -1,6 +0,0 @@
-package com.footballmarketplace.domain.enums;
-
-public enum Role {
-    USER,
-    ADMIN
-}
